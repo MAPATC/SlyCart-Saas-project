30e3c4c8e4c3b56aa47bbfd3da50c32fe1c66542
Vlad <baryga0707@mail.ru>
2026-03-01 17:04:04 +0300

    maybe it's done...?

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -1,7 +1,19 @@
-from aiogram import Router, F
 from lexicons.lexicon import MAIN_MENU
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import (InlineKeyboardButton, 
+                           InlineKeyboardMarkup, 
+                           Message,
+                           CallbackQuery)
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters import CommandStart
 
 router: Router = Router(name=__name__)
 
+def inline_markup() -> InlineKeyboardMarkup:
+    inl_kb_markup =InlineKeyboardBuilder()
+
+    buttons = [InlineKeyboardButton(text=menu['text'], callback_data=menu_cb) 
+               for menu_cb, menu in MAIN_MENU.items()]
+
+    inl_kb_markup.row(*buttons, width=1)
+
+    return inl_kb_markup.as_markup()
--- bot/lexicons/lexicon.py
+++ bot/lexicons/lexicon.py
@@ -1,3 +1,9 @@
+from typing import TypedDict
+
+class MenuValue(TypedDict):
+    text: str
+    description: str
+
 DESCRIPTION: dict[str, str] = {
     "about_description": "Прикольное название(потом)",
     "tariff_description": "Тарифы(чуть позже)",
@@ -6,9 +12,13 @@ DESCRIPTION: dict[str, str] = {
 }
 
 
-MAIN_MENU: dict[str, dict[str, str]] = {
+MAIN_MENU: dict[str, MenuValue] = {
     "about": {"text" : "О нас", "description": DESCRIPTION['about_description']},
     "tariff": {"text": "Тарифы", "description": DESCRIPTION['tariff_description']},
     "reg": {"text": "Регистрация", "description": DESCRIPTION['reg_description']},
     "run_webapp": {"text": "Зайти в магазин", "description": DESCRIPTION['webapp']} 
 }
+
+if __name__ == "__main__":
+    for menu_cb, menu in MAIN_MENU.items():
+        print(menu_cb, menu['text'], menu['description']) 
