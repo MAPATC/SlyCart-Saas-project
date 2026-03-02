8f9fa35d0e0ac47f5586d0518424ae1600a80e97
Vlad <baryga0707@mail.ru>
2026-03-02 21:53:08 +0300

    no changes

--- bot/main.py
+++ bot/main.py
@@ -5,21 +5,24 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config.config import Config, load_config
+from handlers import main_menu
 
-
-config: Config = load_config()
-
-bot = Bot(token=config.bot.token,
-          default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher()
 
 async def main():
+
+    config: Config = load_config()
+
+    bot = Bot(token=config.bot.token,
+            default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    dp = Dispatcher()
 
     logging.basicConfig(
         level=config.log.log_level,
         format=config.log.log_format
     )
 
+    dp.include_router(main_menu.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
