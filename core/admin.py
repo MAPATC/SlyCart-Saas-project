30c7d4065a035cb5214d2866759d190404384d1a
Vlad <baryga0707@mail.ru>
2026-03-16 16:59:41 +0300

    add tables to django-admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TelegramUser, Tariff, Shop, Product
 
 # Register your models here.
+admin.site.register(TelegramUser)
+admin.site.register(Tariff)
+admin.site.register(Shop)
+admin.site.register(Product)
