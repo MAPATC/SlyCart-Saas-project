d84f4b347c62dc2ea0eda7fd9f60251c3aa2c4ee
Vlad <baryga0707@mail.ru>
2026-03-16 16:09:48 +0300

    ORM is done

--- core/models.py
+++ core/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 
 # Create your models here.
 class TelegramUser(models.Model):
-    user_id = models.BigIntegerField(unique=True)
+    user_id = models.BigIntegerField(unique=True, verbose_name="ID телеграмм пользователя")
 
     ROLE_CHOICES = [
         ('customer', 'Покупатель'),
@@ -13,8 +13,14 @@ class TelegramUser(models.Model):
     role = models.CharField(
         max_length=10,
         choices=ROLE_CHOICES, # Первое для базы(customer), второе для админки(Покупатель)
-        default='customer'
+        default='customer',
+        verbose_name="Роль"
     )
+
+    
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи" 
 
     reg_date = models.DateField(auto_now_add=True) #Автодобавление даты регистрации
 
@@ -23,14 +29,49 @@ class TelegramUser(models.Model):
     
 
 class Tariff(models.Model):
-    plan = models.CharField(max_length=50)
-    limits = models.PositiveIntegerField(default=5)
+    plan = models.CharField(max_length=50, verbose_name="Тариф")
+    limits = models.PositiveIntegerField(verbose_name="Лимиты")
     # max_digit - максимальное количество цифр в числе, decimal_places - знаки после запятой
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена тарифа")
+
 
     class Meta:
         verbose_name = "Тариф"
         verbose_name_plural = "Тарифы" # Чтобы в админке не было "Тарифs"
 
     def __str__(self):
-        return f'{self.tariff} -> {self.price}:{self.limits} '
+        return f'{self.plan} -> {self.price}:{self.limits} '
+    
+    
+class Shop(models.Model):
+    owner = models.ForeignKey(TelegramUser, on_delete=models.CASCADE, verbose_name="Владелец") # Если удалим владельца, удалиться вся информация о магазине(CASCADE)
+    # Все о пользователе(тариф, дата регистрации, телеграм айди и т.д)
+    # on_delete работает на тех, на кого мы ссылаемся
+    tariff = models.ForeignKey(Tariff, on_delete=models.SET_NULL, null=True, verbose_name="Тариф") # Если удалим магазин, то просто поставим NULL без удаления всех данных о тарифе
+    # Все о тарифах
+    shop_name = models.CharField(max_length=100, verbose_name="Название магазина")
+    shop_link = models.SlugField(unique=True, verbose_name="Ссылка (slug)")
+
+
+    class Meta:
+        verbose_name = "Магазин"
+        verbose_name_plural = "Магазины"
+        
+    def __str__(self):
+        return f"{self.owner}:{self.tariff} ({self.shop_name} link = {self.shop_link})"
+    
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    # Если удалят магазин, вся информация о продуктах тоже удалиться
+    title = models.CharField(max_length=100, verbose_name="Название товара")
+    description = models.TextField(verbose_name="Описание товара")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена товара")
+    is_active = models.BooleanField(verbose_name="Доступен ли товар", default=True)
+    stock = models.PositiveIntegerField(verbose_name="Осталось товара", default=0)
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+    def __str__(self):
+        return f"{self.title}:{self.price}:{self.stock} is_active:{self.is_active}"
