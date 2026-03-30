29be4675db1a46db51d7c9141867dcd7dffa990f
Vlad <baryga0707@mail.ru>
2026-03-30 23:19:54 +0300

    hello test

--- core/api.py
+++ core/api.py
@@ -3,4 +3,10 @@ from .models import Product
 
 core_router = Router()
 
+
+@core_router.get("hello/")
+def hello(request):
+    return {"message": "Hello from Core"}
+
+
 # TODO: сделать api для одной из моделей(начнем с product)
