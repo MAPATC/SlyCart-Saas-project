68eaade8f461ece5af64b76a280163ac8dbc6ffe
Vlad <baryga0707@mail.ru>
2026-04-01 22:21:59 +0300

    update admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import TelegramUser, Tariff, Shop, Product, CartItem, Order, OrderItem, ProductImage
+from .models import TelegramUser, Tariff, CustomerProfile, OwnerProfile, Shop, Product, CartItem, Order, OrderItem, ProductImage
 
 # Register your models here.
 admin.site.register(TelegramUser)
+admin.site.register(CustomerProfile)
+admin.site.register(OwnerProfile)
 admin.site.register(Tariff)
 admin.site.register(Shop)
 admin.site.register(Product)
@@ -10,4 +12,5 @@ admin.site.register(CartItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ProductImage)
+
 
