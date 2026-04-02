5a7773e074b4a4d32793e3c2a37da0a791d0caf2
Vlad <baryga0707@mail.ru>
2026-04-02 20:12:15 +0300

    get tguser

--- core/api.py
+++ core/api.py
@@ -1,12 +1,15 @@
 from ninja import Router
-from .models import Product
+from .models import TelegramUser
+from .schemas import TelegramUserOut
 
 core_router = Router()
 
+# Эндпоинты (Endpoints) — это, по сути, «адреса», 
+# по которым твой фронтенд (или Telegram-бот) будет обращаться к твоему бэкенду за данными.
 
-@core_router.get("hello/")
-def hello(request, name=None):
-    return {"message": f"{name}"}
-
+@core_router.get("/hello/{user_id}", response=TelegramUserOut) # Что то похожее на эндпоинт
+def get_user(request, user_id: int): # Что будет принимать функция
+    user = TelegramUser.objects.get(user_id=user_id)
+    return user
 
 # TODO: сделать api для одной из моделей(начнем с product)
