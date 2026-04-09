03df3e1ba899e6f14441220ef83145f32bbd39e4
Vlad <baryga0707@mail.ru>
2026-04-09 22:02:46 +0300

    custom exceptions

---
+++ core/exceptions.py
@@ -0,0 +1,47 @@
+class BaseServiceException(Exception):
+    """Базовое исключение для всех сервисов нашего проекта"""
+    def __init__(self, message="Произошла ошибка в работе сервиса"):
+        self.message = message
+        super().__init__(self.message)
+
+# Ошибки регистрации
+class UserAlreadyExistsError(BaseServiceException):
+    """Пользователь с таким ID уже есть"""
+    pass
+
+class InvalidRoleError(BaseServiceException):
+    """Передана несуществующая роль"""
+    pass
+
+# Ошибки магазинов и тарифов
+class ShopLimitExceededError(BaseServiceException):
+    """Превышен лимит создания магазинов для текущего тарифа"""
+    pass
+
+class ProductLimitExceededError(BaseServiceException):
+    """Превышен лимит создания товаров для текущего тарифа"""
+    pass
+
+class BrandAlreadyTakenError(BaseServiceException):
+    """Название бренда уже занято другим владельцем"""
+    pass
+
+class InvalidOrderStatusError(BaseServiceException):
+    """Несуществующий статус заказа"""
+    pass
+
+class EmptyPhoneNumberError(BaseServiceException):
+    """Пустой номер телефона у покупателя"""
+    pass
+
+class PhoneNumberAlreadyTakenError(BaseServiceException):
+    """Такой номер телефона уже занят"""
+    pass
+
+class ShopAlreadyExists(BaseServiceException):
+    """Такой магазин уже существует"""
+    pass
+
+class NegativePriceError(BaseServiceException):
+    """Отрицательная цена для товара"""
+    pass
