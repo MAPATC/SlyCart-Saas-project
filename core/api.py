b79db5e3af35cc212cf91fa8d92faade4ac42d5f
Vlad <baryga0707@mail.ru>
2026-04-11 15:37:10 +0300

    product endpoint

--- core/api.py
+++ core/api.py
@@ -1,11 +1,14 @@
 from ninja import Router
+from typing import List
 from django.shortcuts import get_object_or_404
-from .models import TelegramUser, OwnerProfile
-from .services import create_user, create_shop
+from .models import TelegramUser, OwnerProfile, Product, Shop
+from .services import create_user, create_shop, create_product
 from .schemas import (TelegramUserOut, 
                       TelegramUserIn,
                       ShopIn,
-                      ShopOut)
+                      ShopOut,
+                      ProductIn,
+                      ProductOut)
 
 core_router = Router()
 
@@ -33,10 +36,33 @@ def create_shop_endpoint(request, data: ShopIn):
 
     shop = create_shop(
         user=owner,
+        name=data.shop_name,
         link=data.shop_link
     )
 
     return shop
 
+@core_router.post("/product", response=ProductOut)
+def create_product_endpoint(request, data: ProductIn):
+
+    shop_obj = get_object_or_404(Shop, id=data.shop, owner__owner__user_id=data.user_id)
+
+    product = create_product(
+        shop=shop_obj,
+        title=data.title,
+        description=data.description,
+        price=data.price,
+        stock=data.stock,
+        is_active=data.is_active
+    )
+
+    return product
+
+@core_router.get("/my-shops", response=List[ShopOut])
+def list_my_shops(request, user_id: int):
+
+    shops = Shop.objects.filter(owner__owner__user_id=user_id)
+
+    return shops
 
 # TODO: эндпоинт для товаров
