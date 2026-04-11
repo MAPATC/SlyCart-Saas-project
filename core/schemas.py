20c43ef85973e103f27d6fd2b128567efbecd420
Vlad <baryga0707@mail.ru>
2026-04-11 15:38:00 +0300

    product schemas

--- core/schemas.py
+++ core/schemas.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
+from pydantic import PositiveInt
 import uuid
 from ninja import Schema, ModelSchema
 from datetime import date
-from .models import TelegramUser, Shop
+from .models import TelegramUser, Shop, Product
 
 class TelegramUserIn(Schema): # Что мы будем вносить
     user_id: int
@@ -19,10 +21,35 @@ class TelegramUserOut(ModelSchema): # Что мы будет отдавать в
 
 class ShopIn(Schema):
     owner: int
+    shop_name: str
     shop_link: str = None
 
 
 class ShopOut(ModelSchema):
+    # ModelSchema сделает все сама
+    owner_id: int  # Это "дополнительное" поле для JSON
+
     class Meta:
         model = Shop
-        fields = ['id', 'owner','shop_name', 'shop_link']
+        fields = ['id', 'shop_name', 'shop_link'] # Здесь ТОЛЬКО реальные колонки из таблицы Shop
+
+    # 3. Твой "добытчик" (теперь он сработает)
+    @staticmethod
+    def resolve_owner_id(obj):
+        return obj.owner.owner.user_id
+
+
+class ProductIn(Schema):
+    shop: int
+    user_id: int
+    title: str
+    description: str
+    price: Decimal
+    stock: PositiveInt
+    is_active: bool = True
+
+
+class ProductOut(ModelSchema):
+    class Meta:
+        model = Product
+        fields = ["title", "description", "price", "stock", "is_active"]
