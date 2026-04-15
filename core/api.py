4fb48744e8a8d096b320bbb73df82a5f9254dd20
Vlad <baryga0707@mail.ru>
2026-04-15 22:44:46 +0300

    mpv is done

--- core/api.py
+++ core/api.py
@@ -1,5 +1,6 @@
 import uuid
 from ninja import Router, File
+from ninja.pagination import paginate
 from ninja.files import UploadedFile
 from typing import List
 from django.shortcuts import get_object_or_404
@@ -7,13 +8,15 @@ from django.shortcuts import get_object_or_404
 from .exceptions import NegativePriceError
 from .models import TelegramUser, OwnerProfile, Product, Shop
 from .services import create_user, create_shop, create_product
-from .schemas import (TelegramUserOut, 
-                      TelegramUserIn,
-                      ShopIn,
-                      ShopOut,
-                      ProductIn,
-                      ProductOut,
-                      ProductPatch)
+from .schemas import (
+    TelegramUserOut, 
+    TelegramUserIn,
+    ShopIn,
+    ShopOut,
+    ProductIn,
+    ProductOut,
+    ProductPatch
+    )
 
 core_router = Router()
 
@@ -37,7 +40,10 @@ def create_user_endpoint(request, data: TelegramUserIn): # Что будет п�
 @core_router.post('/shop', response=ShopOut)
 def create_shop_endpoint(request, data: ShopIn):
 
-    owner = get_object_or_404(OwnerProfile, owner__user_id=data.owner)
+    owner = get_object_or_404(
+        OwnerProfile, 
+        owner__user_id=data.owner
+        )
 
     shop = create_shop(
         user=owner,
@@ -50,7 +56,11 @@ def create_shop_endpoint(request, data: ShopIn):
 @core_router.post("/product", response=ProductOut)
 def create_product_endpoint(request, data: ProductIn, images: List[UploadedFile] = File()):
 
-    shop_obj = get_object_or_404(Shop, id=data.shop, owner__owner__user_id=data.user_id)
+    shop_obj = get_object_or_404(
+        Shop, 
+        id=data.shop, 
+        owner__owner__user_id=data.user_id
+        )
 
     product = create_product(
         shop=shop_obj,
@@ -65,7 +75,7 @@ def create_product_endpoint(request, data: ProductIn, images: List[UploadedFile]
     return product
 
 @core_router.get("/my-shops", response=List[ShopOut])
-def list_my_shops(request, user_id: int):
+def list_my_shops_endpoint(request, user_id: int):
 
     return Shop.objects.filter(owner__owner__user_id=user_id).select_related('owner__owner')
 
@@ -86,7 +96,7 @@ def edit_products_endpoint(request, product_id: int , data: ProductPatch):
 
     update_data = data.dict(exclude_unset=True)
 
-    update_data.pop("user_id")
+    update_data.pop("user_id", None)
 
     for field, value in update_data.items():
         if field == "price" and value < 0:
@@ -97,4 +107,22 @@ def edit_products_endpoint(request, product_id: int , data: ProductPatch):
     product.save()
 
     return product
-# TODO: эндпоинт для товаров
+
+@core_router.get("/products", response=List[ProductOut])
+@paginate
+def list_products_endpoint(request):
+    return Product.objects.all()
+
+@core_router.delete("/product/{product_id}", response={204: None})
+def delete_product_endpoint(request, product_id: int, owner_id: int):
+    
+    product = get_object_or_404(
+        Product,
+        id=product_id,
+        shop__owner__owner__user_id=owner_id
+    )
+
+    product.delete()
+
+    return 204, None
+
