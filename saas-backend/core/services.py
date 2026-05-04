113524347acc60a6b0dcaa2ae51c9310884e5363
Vlad <baryga0707@mail.ru>
2026-05-04 17:43:17 +0300

    fix: useless import

--- saas-backend/core/services.py
+++ saas-backend/core/services.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from typing import List
 from ninja.files import UploadedFile
-from django.db import transaction, IntegrityError
+from django.db import transaction
 from django.db import models
 from django.db.models import F
 from django.utils.translation import gettext_lazy as _
