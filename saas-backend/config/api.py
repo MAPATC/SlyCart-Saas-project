5161b922d5608ce81b9d0e2500bd4be671ce6c51
Vlad <baryga0707@mail.ru>
2026-05-04 22:20:18 +0300

    feat: exceptions handlers and NinjaJSONEncoder

--- saas-backend/config/api.py
+++ saas-backend/config/api.py
@@ -1,7 +1,59 @@
+from datetime import datetime
+import json
+import uuid
+
 from ninja import NinjaAPI
 from core.api import core_router
+from ninja.renderers import JSONRenderer
+from core.exceptions import UserAlreadyExistsError, PhoneNumberAlreadyTakenError
+from ninja.responses import NinjaJSONEncoder
 
-api = NinjaAPI()
+class UnicodeJSONRenderer(JSONRenderer):
+    def render(self, request, data, *, response_status):
+        return json.dumps(
+            data, 
+            cls=NinjaJSONEncoder, # добавили поддержку дат и чисел, так как базовый json это не поддерживает
+            ensure_ascii=False
+        ).encode("utf-8")
+
+
+api = NinjaAPI(renderer=UnicodeJSONRenderer())
+
+@api.exception_handler(UserAlreadyExistsError)
+def user_already_exists_handler(request, exc):
+
+    return api.create_response(
+        request=request,
+        data={
+            "detail": str(exc),
+            "timestamp": datetime.now(),      # Сработает благодаря NinjaJSONEncoder
+            "error_id": uuid.uuid4(),         # Сработает благодаря NinjaJSONEncoder
+            "method": request.method,
+            "is_authenticated": request.user.is_authenticated, 
+            "user_status": "Guest" if request.user.is_anonymous else "Member",
+            "path": request.path
+            },
+        status=400
+    )
+
+@api.exception_handler(PhoneNumberAlreadyTakenError)
+def user_already_exists_handler(request, exc):
+
+    return api.create_response(
+        request=request,
+        data={
+            "detail": str(exc),
+            "timestamp": datetime.now(),      # Сработает благодаря NinjaJSONEncoder
+            "error_id": uuid.uuid4(),         # Сработает благодаря NinjaJSONEncoder
+            "method": request.method,
+            "is_authenticated": request.user.is_authenticated, 
+            "user_status": "Guest" if request.user.is_anonymous else "Member",
+            "path": request.path
+            },
+        status=400
+    )
+
 
 api.add_router("/core/", core_router)
+
 
